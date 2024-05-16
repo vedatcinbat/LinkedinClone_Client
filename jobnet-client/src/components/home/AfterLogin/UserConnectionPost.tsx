261f8dce27577125d28cfda38c2dd888ddb669b5
Vedat Cinbat <cinbatvedat@gmail.com>
2024-05-16 23:37:34 +0300

    - UserConnectionPost design updated

--- jobnet-client/src/components/home/AfterLogin/UserConnectionPost.tsx
+++ jobnet-client/src/components/home/AfterLogin/UserConnectionPost.tsx
@@ -7,11 +7,23 @@ interface UserConnectionPostProps {
 
 const UserConnectionPost: React.FC<UserConnectionPostProps> = ({post}) => {
     return(
-        <div className="w-[60vh] h-[30vh] bg-gray8 text-white mt-2 mb-4 rounded-xl">
-            <div>{post.publishTime.toString()}</div>
-            <div>{post.textContent}</div>
-            <div>{post.user?.firstname} {post.user?.lastname}</div>
-            <div>{post.user?.company?.companyName}</div>
+        <div className="w-[60vh] h-[30vh] flex flex-col items-center bg-gray7 text-white mt-2 mb-4 rounded-xl cursor-pointer">
+            <div className="userDetails w-full h-[20%] bg-black2 rounded-tl-xl rounded-tr-xl text-white flex justify-between items-center p-2">
+                <div className="fullNameCompany flex items-center gap-2">
+                    <div className="text-lg text-gray4">{post.user?.firstname} {post.user?.lastname}</div>
+                    <div className="text-sm text-gray6">-{post.user?.company && post.user.company.companyName}-</div>
+                </div>
+                <div className="publishTime">
+                    <div className="text-xs">({post.publishTime.toString()})</div>
+                </div>
+            </div>
+            <div className="content w-full h-[70%] bg-commentBg flex justify-center items-center">
+                <div>{post.textContent}</div>
+            </div>
+            <div className="likesComments w-full flex gap-2 justify-start items-center p-1">
+                <div className="hover:text-black cursor-pointer">Likes</div>
+                <div className="hover:text-black cursor-pointer">Comments</div>
+            </div>
         </div>
     )
 }
